6ce7e038914fd03878e6d5c90ce7e6d9afb4af6e
ahmet umut akkaş <95590170+ahmet-umut@users.noreply.github.com>
2024-11-01 14:00:47 +0300

    sender and receiver finished

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,9 @@
 import scapy
+from scapy.all import sniff, IP, ICMP, Packet
 
 # Implement your ICMP receiver here
+
+def icmp_packet_callback(packet):
+	if IP in packet and ICMP in packet and packet[IP].ttl == 1:
+		packet.show()
+#sniff(filter="icmp", prn=icmp_packet_callback, store=0)
--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,10 @@
 import scapy
+from scapy.all import IP, ICMP, send
 
 # Implement your ICMP sender here
+
+def send_icmp_packet(destination):
+	packet = IP(dst=destination, ttl=1) / ICMP()
+	send(packet)
+
+send_icmp_packet("receiver")
